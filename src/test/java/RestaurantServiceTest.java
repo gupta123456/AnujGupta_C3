c9a487bc2eb4822dc659ff21447e60c13f865a62
gupta123456 <anujg0046@gmail.com>
2023-02-28 23:40:11 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantServiceTest.java
+++ src/test/java/RestaurantServiceTest.java
@@ -27,7 +27,13 @@ class RestaurantServiceTest {
     //You may watch the video by Muthukumaran on how to write exceptions in Course 3: Testing and Version control: Optional content
     @Test
     public void searching_for_non_existing_restaurant_should_throw_exception() throws restaurantNotFoundException {
-        
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        service.findRestaurantByName("Amelie's cafe");
+        assertThrows(restaurantNotFoundException.class,()->service.findRestaurantByName("Amelie's cafe"));
     }
     //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
 
--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -24,7 +24,13 @@ class RestaurantTest {
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
-
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertThrows(itemNotFoundException.class,()->LocalTime.now());
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
